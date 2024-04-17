8ddb52218b4fc7b46f24a75d0a7a1e7c75b3133c
Yugyeong Jo <yugyeongjo1215@gmail.com>
2024-04-17 19:56:10 +0900

    update

---
+++ my-app/src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<div class="container">
+    <nav>
+        <ul>
+            <li><a href="#" class="secondary">...</a></li>
+        </ul>
+        <ul>
+            <li><strong>Acme Corp</strong></li>
+        </ul>
+        <ul>
+            <li><a href="#" class="secondary">...</a></li>
+        </ul>
+    </nav>
+    
+    <slot />
+</div>
