72dc8c5de7bbfcfae9dd365f401d976c8a56ea72
YugyeongJo <yugyeongjo1215@gmail.com>
2024-05-04 22:44:21 +0900

    add

--- my-app/src/lib/pocketbase.js
+++ my-app/src/lib/pocketbase.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-// place files you want to import through the `$lib` alias in this folder.
->>>>>>> 1e17e289df183efff772d020d89406cce2a18882
 import { PUBLIC_POCKETBASE_URL } from '$env/static/public';
 import PocketBase from 'pocketbase';
 
